2b457acb3b9e4af3defaf72db7d0b1377d150e67
Rafael Carvalho <rapha.pse@outlook.com>
2020-02-05 22:05:33 -0300

    Finished input

---
+++ src/components/Input.vue
@@ -0,0 +1,36 @@
+<template>
+ <div id="input">
+    <div class="form-group">
+      <label :for="id">{{ label }}</label>
+      <input 
+        type="text" 
+        :id="id" 
+        :name="id"
+        :maxlength="maxLength" 
+        placeholder="" 
+        :value="dataValue"
+        @input="sendValue">
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: String,
+    dataValue: [Number, String],
+    label: null,
+    maxLength: null
+  },
+  data() {
+    return {
+      valueInput: this.dataValue
+    }
+  },
+  methods: {
+    sendValue(event) {
+      this.$emit('input', event.target.value);
+    }
+  }
+}
+</script>
--- src/views/FormBoard.vue
+++ src/views/FormBoard.vue
@@ -1,6 +1,47 @@
 <template>
-  <div id="form">
-    <h1>Form Board Add User</h1>
-  </div>
+  <form id="form"  @submit.prevent="sendForm">
+      <h1>Cadastrar</h1>
+      <Input
+        label="Usuário no Github"
+        id="input-label"
+        maxLength="100"
+        v-model="formDev.user"
+      />
+
+      <Input
+        class="tcn"
+        label="Tecnologia"
+        id="input-label"
+        maxLength="130"
+        v-model="formDev.tecnical"
+      />
+    <button class="btn-submit" type="submit">SEND</button>
+  </form>
 </template>
-//  max carcter 130
+
+
+<script>
+
+import Input from '../components/Input.vue';
+
+export default {
+  components: {
+    Input
+  },
+  data() {
+    return {
+      formDev: {
+        user: null,
+        tecnical: null,
+        lat: null,
+        long: null
+      }
+    }
+  },
+  methods: {
+    sendForm() {
+      console.log(' data ', this.formDev);
+    }
+  }
+}
+</script>
